235b8058cf139aa7795760a6f7dd0fcca903ad85
moiz-xyz <a.moizniaxi123@gmail.com>
2025-03-05 23:24:14 +0500

    nav bar animation made

---
+++ 03_Navbar Animations/main.js
@@ -0,0 +1,20 @@
+let tl = gsap.timeline();
+tl.from ("h2" , {
+    y:-20,
+    opacity: 0,
+    duration : 1,
+    delay : 0.5
+})
+tl.from ("h4" , {
+    y:-20,
+    opacity: 0,
+    duration : 1,
+    stagger : 0.3, 
+})
+tl.from ("h1" , {
+    y:20,
+    opacity: 0,
+    duration : 3 ,
+    scale : 0.2,
+    repeat : -1
+})
